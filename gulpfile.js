0a88a62c98f30b59e2fa9807caa25b426f640b88
MediaDUK <hello@mediaduk.com>
2018-02-15 18:25:41 -0800

    removed node_modules from git repo. gulp task for vendor js added.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 const gulp = require('gulp'),
   browserSync = require('browser-sync').create(),
-  sass = require('gulp-sass');
+  sass = require('gulp-sass'),
+  concat = require('gulp-concat'),
+  rename = require('gulp-rename'),
+  uglify = require('gulp-uglify');
+
+// concatenate and uglify
+gulp.task('js', function() {
+  return gulp.src(['node_modules/jquery/dist/jquery.slim.min.js',
+                  'node_modules/tether/dist/js/tether.js',
+                  'node_modules/bootstrap/dist/js/bootstrap.min.js'],
+    { base:'node_modules'} )
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest('./assets/js'));
+});
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], () => {
+gulp.task('serve', ['js','sass'], () => {
   browserSync.init({
     server: './'
   });
+  gulp.watch('assets/js/**/*.js', ['js']);
   gulp.watch('assets/scss/*.scss', ['sass']);
   gulp.watch('./*.html').on('change', browserSync.reload);
 });
